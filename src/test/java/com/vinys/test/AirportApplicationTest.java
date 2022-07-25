3cf83cadf70919239ef208db85511c8b05f95b15
RitikGarg7 <you@example.com>
2022-07-25 16:08:07 +0530

    third commit

---
+++ src/test/java/com/vinys/test/AirportApplicationTest.java
@@ -0,0 +1,19 @@
+package com.vinys.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+public class AirportApplicationTest {
+    @Test
+    void testFindAirportByThrowsExceptionWhenInputInvalid(){
+        //do i have code to test ?
+        fail("Not Yet implemented");
+    }
+
+    @Test
+    void testFindAirportByReturnListOfAirportsIfValid(){
+        fail("Not Yet Implemented");
+
+    }
+}
+
